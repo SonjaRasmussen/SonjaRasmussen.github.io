642d246bf0af52cede77603df345bdc7f5c6e213
Sonja Rasmussen <sonja.rasmussen@gmail.com>
2019-04-27 20:13:42 -0700

    Add CORS middleware

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express()
 const port = process.env.PORT
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res)=> res.send("Hello World"))
 
